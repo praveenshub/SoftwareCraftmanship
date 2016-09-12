d0df2d9fa2bd989fb4f0afb2ec89e528fb86b508
Praveen.Dsouza <praveendsouza85@gmail.com>
2016-09-12 11:49:39 +0100

    First check-in

---
+++ FizzBuzz/FizzBuzz/Calculator.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzz
+{
+    internal class Calculator
+    {
+        private const string FIZZ = "Fizz";
+        private const string BUZZ = "Buzz";
+
+        public string Process(int i)
+        {
+            if (IsMultipleOfThree(i))
+            {
+                if (IsMultipleOfFive(i))
+                {
+                    return $"{FIZZ}{BUZZ}";
+                }
+
+                return FIZZ;
+            }
+
+            if (IsMultipleOfFive(i))
+            {
+                return BUZZ;
+            }
+
+            return i.ToString();
+        }
+
+        private static bool IsMultipleOfFive(int i)
+        {
+            return i % 5 == 0;
+        }
+
+        private static bool IsMultipleOfThree(int i)
+        {
+            return i % 3 == 0;
+        }
+    }
+}
---
+++ FizzBuzz/FizzBuzz/FizzBuzzTests.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace FizzBuzz
+{
+    [TestFixture]
+    internal class FizzBuzzTests
+    {
+        private Calculator _calculator;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _calculator = new Calculator();
+        }
+
+        [TestCase(1, "1")]
+        [TestCase(2, "2")]
+        [TestCase(3, "Fizz")]
+        [TestCase(5, "Buzz")]
+        [TestCase(15, "FizzBuzz")]
+        [TestCase(30, "FizzBuzz")]
+        [TestCase(6, "Fizz")]
+        [TestCase(9, "Fizz")]
+        [TestCase(10, "Buzz")]
+        public void NumberOne_Entered_Returns_One(int number, string expected)
+        {
+            var result = _calculator.Process(number);
+
+            Assert.AreEqual(expected, result);
+        }
+    }
+}
