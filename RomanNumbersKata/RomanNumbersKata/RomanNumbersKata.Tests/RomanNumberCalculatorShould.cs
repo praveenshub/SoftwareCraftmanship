964b47c64d0ddcb35869febaf393e2dd56347054
Lewis Jackson <lewis.jackson@asos.com>
2016-09-22 14:00:29 +0100

    Start project RomanNumbersKata

---
+++ RomanNumbersKata/RomanNumbersKata/RomanNumbersKata.Tests/RomanNumberCalculatorShould.cs
@@ -0,0 +1,17 @@
+﻿namespace RomanNumbersKata.Tests
+{
+    using FluentAssertions;
+
+    using NUnit.Framework;
+
+    public class RomanNumberCalculatorShould
+    {
+        [Test]
+        public void ReturnI_Given1()
+        {
+            string romanNumber = RomanNumberCalculator.Convert(1);
+
+            romanNumber.Should().Be("I");
+        } 
+    }
+}
