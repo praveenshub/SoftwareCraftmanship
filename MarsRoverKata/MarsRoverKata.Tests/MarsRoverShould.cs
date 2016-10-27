daee98855f4f4cd297072a40cf6cbda824c86a91
Praveen.Dsouza <praveendsouza85@gmail.com>
2016-10-27 14:46:21 +0100

    Refactor Tests

--- MarsRoverKata/MarsRoverKata.Tests/MarsRoverShould.cs
+++ MarsRoverKata/MarsRoverKata.Tests/MarsRoverShould.cs
@@ -20,15 +20,6 @@ namespace MarsRoverKata.Tests
         [TestCase("5 5", "0 0 N", "M", "0 1 N")]
         [TestCase("5 5", "0 0 N", "MM", "0 2 N")]
         [TestCase("5 5", "0 0 N", "MMM", "0 3 N")]
-        public void Return_FinalCoordinatesAndHeading_Given_MovedForwardOneGridPoint_AndMaintainTheSameHeading(string sizeOfThePlateau, string positionOfTheRover, string instructions, string expected)
-        {
-            MarsRover marsRover = new MarsRover();
-
-            string finalCoordinates = marsRover.Move(sizeOfThePlateau, positionOfTheRover, instructions);
-
-            finalCoordinates.Should().Be(expected);
-        }
-
         [TestCase("5 5", "0 0 E", "M", "1 0 E")]
         [TestCase("5 5", "0 0 E", "MM", "2 0 E")]
         [TestCase("5 5", "1 1 E", "M", "2 1 E")]
@@ -36,7 +27,7 @@ namespace MarsRoverKata.Tests
         [TestCase("5 5", "1 1 E", "MMM", "4 1 E")]
         [TestCase("5 5", "2 1 E", "M", "3 1 E")]
         [TestCase("5 5", "2 1 E", "MM", "4 1 E")]
-        public void Return_FinalCoordinatesAndHeading_Given_MovedForwardSomeGridPoints_AndMaintainTheSameHeading(string sizeOfThePlateau, string positionOfTheRover, string instructions, string expected)
+        public void Return_FinalCoordinatesAndHeading_Given_MovedForwardOneGridPoint_AndMaintainTheSameHeading(string sizeOfThePlateau, string positionOfTheRover, string instructions, string expected)
         {
             MarsRover marsRover = new MarsRover();
 
