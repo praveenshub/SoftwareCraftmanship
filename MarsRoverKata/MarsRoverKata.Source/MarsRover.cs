e6aaa7c7cc1615d59a10dc5c6de052edc09b0591
Praveen.Dsouza <praveendsouza85@gmail.com>
2016-10-27 14:00:34 +0100

    RoverMovesUpto3GridPointsWhileMaintainingSameDirection

---
+++ MarsRoverKata/MarsRoverKata.Source/MarsRover.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace MarsRoverKata.Source
+{
+    public class MarsRover
+    {
+        public MarsRover()
+        {
+            
+        }
+
+        public string Move(string sizeOfThePlateau, string positionOfTheRover, string instructions)
+        {
+            int numberOfForwardsMoves = instructions.Length;
+            return String.Format("0 {0} N", numberOfForwardsMoves);
+        }
+    }
+}
---
+++ MarsRoverKata/MarsRoverKata.Tests/MarsRoverShould.cs
@@ -0,0 +1,59 @@
+﻿using FluentAssertions;
+using MarsRoverKata.Source;
+using NUnit.Framework;
+
+namespace MarsRoverKata.Tests
+{
+    [TestFixture]
+    public class MarsRoverShould
+    {
+        [Test]
+        public void Return_FinalCoordinatesAndHeading_Given_NoInputIsSent()
+        {
+            MarsRover marsRover = new MarsRover();
+
+            string startingCoordinates = marsRover.Move("5 5", "0 0 N", string.Empty);
+
+            startingCoordinates.Should().Be("0 0 N");
+        }
+
+        [Test]
+        public void Return_FinalCoordinatesAndHeading_Given_MovedForwardOneGridPoint_AndMaintainTheSameHeading()
+        {
+            string sizeOfThePlateau = "5 5";
+            string positionOfTheRover = "0 0 N";
+            string instructions = "M";
+            MarsRover marsRover = new MarsRover();
+
+            string finalCoordinates = marsRover.Move(sizeOfThePlateau, positionOfTheRover, instructions);
+
+            finalCoordinates.Should().Be("0 1 N");
+        }
+
+        [Test]
+        public void Return_FinalCoordinatesAndHeading_Given_MovedForwardTwoGridPoints_AndMaintainTheSameHeading()
+        {
+            string sizeOfThePlateau = "5 5";
+            string positionOfTheRover = "0 0 N";
+            string instructions = "MM";
+            MarsRover marsRover = new MarsRover();
+
+            string finalCoordinates = marsRover.Move(sizeOfThePlateau, positionOfTheRover, instructions);
+
+            finalCoordinates.Should().Be("0 2 N");
+        }
+
+        [Test]
+        public void Return_FinalCoordinatesAndHeading_Given_MovedForwardThreeGridPoints_AndMaintainTheSameHeading()
+        {
+            string sizeOfThePlateau = "5 5";
+            string positionOfTheRover = "0 0 N";
+            string instructions = "MMM";
+            MarsRover marsRover = new MarsRover();
+
+            string finalCoordinates = marsRover.Move(sizeOfThePlateau, positionOfTheRover, instructions);
+
+            finalCoordinates.Should().Be("0 3 N");
+        }
+    }
+}
